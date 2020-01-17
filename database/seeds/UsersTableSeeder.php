dc957d79a9972a6909541d0dd5ac8edd542f9032
MikeHanon <mike.hanon@gmail.com>
2020-01-17 13:34:02 +0100

    commit #1

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        DB::table('role_user')->truncate();
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $sellerRole = Role::where('name', 'seller')->first();
+        $buyerRole = Role::where('name', 'buyer')->first();
+
+        $admin = User::create([
+            'name'=> 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password')
+        ]);
+        $seller = User::create([
+            'name'=> 'Seller User',
+            'email' => 'seller@seller.com',
+            'password' => Hash::make('password')
+        ]);
+        $buyer = User::create([
+            'name'=> 'Buyer User',
+            'email' => 'buyer@buyer.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $admin->roles()->attach($adminRole);
+        $seller->roles()->attach($sellerRole);
+        $buyer->roles()->attach($buyerRole);
+    }
+}
