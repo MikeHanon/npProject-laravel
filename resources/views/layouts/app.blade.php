ac6734928943b8075931caa8ce77992598db9605
MikeHanon <mike.hanon@gmail.com>
2020-01-21 13:50:58 +0100

    add navbar + css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <img src="{{ asset('images/logo.png') }}" alt="" /> 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -49,7 +49,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -61,16 +61,31 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item dropdown">
+                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    category
+                                 </a>
+                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="index.php?action=category&id=1">Alimentation</a>
+                                    <a class="dropdown-item" href="index.php?action=category&id=2">Batiment</a>
+                                    <a class="dropdown-item" href="index.php?action=category&id=3">Fabrication</a>
+                                    <a class="dropdown-item" href="index.php?action=category&id=4">Service</a>
+                                 </div>
+                              </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }} 
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="index.php?action=profile">Votre Compte</a>
+                                    <a class="dropdown-item" href="index.php?action=panier">commande</a>
+                                    <div class="dropdown-divider"></div>
+                                   
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('déconnexion') }}
                                     </a>
                                     @can('manage-users')
                                     <a href="{{route('admin.users.index')}}" class="dropdown-item"> Gestion Admin</a>
@@ -88,11 +103,98 @@
         </nav>
 
         <main class="py-4">
-            <div class="container">
+         
             @include('partials.alert')
             @yield('content')
-            </div>
+          
         </main>
+      
+        <footer>
+      <div class="main-footer">
+         <div class="container">
+            <div class="row">
+
+               <div class="footer-link-box clearfix">
+                  <div class="col-md-6 col-sm-6">
+                     <div class="left-f-box">
+                        <div class="col-sm-4">
+                           <h2>Vendre sur E-artisanat</h2>
+                           <ul>
+                              <li><a href="index.php?action=register">Crer un compte</a></li>
+                              <li><a href="index.php?action=faq">FAQ pour vendeur</a></li>
+                           </ul>
+                        </div>
+                        <div class="col-sm-4">
+                           <h2>Acheter sur E-artisanat</h2>
+                           <ul>
+                              <li><a href="index.php?action=register">Crer un compte</a></li>
+                              <li><a href="index.php?action=faq">FAQ pour acheteur</a></li>
+                           </ul>
+                        </div>
+                        <div class="col-sm-4">
+                           <h2>COMPANY</h2>
+                           <ul>
+                              <li><a href="index.php?action=apropos">A propos de E-artisanat</a></li>
+                              <li><a href="index.php?action=contact">Contactez-nous</a></li>
+
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-sm-6">
+                     <div class="right-f-box">
+                        <h2>category</h2>
+                        <ul class="col-sm-4">
+                          <li> <a  href="index.php?action=category&id=1">Alimentation</a></li>
+                          <li><a  href="index.php?action=category&id=2">Batiment</a></li>
+                          <li> <a  href="index.php?action=category&id=3">Fabrication</a></li>
+                          <li>  <a  href="index.php?action=category&id=4">Service</a></li>
+                        </ul>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="copyright">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-8">
+                  <p><img width="90" src="images/logo.png" alt="#" style="margin-top: -5px;" /> All Rights Reserved. Company Name © 2018</p>
+               </div>
+               <div class="col-md-4">
+                  <ul class="list-inline socials">
+                     <li>
+                        <a href="">
+                           <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="">
+                           <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="">
+                           <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="#">
+                           <i class="fa fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                     </li>
+                  </ul>
+                  <!-- <ul class="right-flag">
+                        <li><a href="#"><img src="images/flag.png" alt="" /> <span>Change</span></a></li>
+                     </ul> -->
+               </div>
+            </div>
+         </div>
+      </div>
+   </footer>
     </div>
+    @yield('script')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,171 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+@section('content')
+<div class="main-product">
+    <div class="container">
+        <div class="row clearfix">
+            <div class="find-box">
+                <h1>Bienvenu sur E-artisanat</h1>
+                <h4>le premier site d'économie collaborative sur l'artisanat, que se soit pour l'alimentation, le batiments, la fabrication
+                    d'objet ou même de petit service vous trouverez tout chez nous
+                </h4>
+                <div class="row clearfix">
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                        <a href="index.php?action=category&id=1">
+                            <div class="box-img">
+                                <h4>Alimentation</h4>
+                                <img src="{{ url('/images/product/allimentation.png') }}" alt="" />
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                        <a href="index.php?action=category&id=2"">
+                            <div class="box-img">
+                                <h4>Batiment</h4>
+                                <img src="{{ url('/images/product/batiment.png') }}" alt="" />
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                        <a href="index.php?action=category&id=3">
+                            <div class="box-img">
+                                <h4>Fabrication</h4>
+                                <img src="{{ url('/images/product/fabrication.png') }}" alt="" />
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-md-3">
+                        <a href="index.php?action=category&id=4">
+                            <div class="box-img">
+                                <h4>Services</h4>
+                                <img src="{{ url('/images/product/service.png') }}" alt="" />
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+<div class="cat-main-box">
+    <div class="container">
+        <div class="row panel-row">
+            <div class="col-md-4 col-sm-6 wow fadeIn" data-wow-delay="0.0s">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <img src="{{ url('/images/xpann-icon.jpg') }}" class="icon-small" alt="">
+                        <h4>Entreprenez</h4>
+                        <p>Réaliser des profits jusqu'à 5100 €/an sans taxe <a href="index.php?action=faq">en savoire plus</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 wow fadeIn" data-wow-delay="0.2s">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <img src="{{ url('/images/create-icon.jpg') }}" class="icon-small" alt="">
+                        <h4>Achetez</h4>
+                        <p>Acheté de vrai produit artisanaux fais près de chez vous. Des siège, bijoux ou même de bon petit plat
+                            <a href="index.php?action=faq">en savoire plus</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 wow fadeIn hidden-sm" data-wow-delay="0.4s">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <img src="{{ url('/images/get-icon.jpg') }}" class="icon-small" alt="">
+                        <h4>Soyez inspirer</h4>
+                        <p>Laissé aller votre inspiration pour proposer vos plus belle création <a href="index.php?action=faq">en savoire plus</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="start-free-box">
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <div class="main-start-box">
+                    <div class="free-box-a clearfix">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="left-a-f">
+                                <h3>E-artisanat, une platforme construite pour vous aider à grandir sur le web</h3>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="right-a-f">
+                                <p>Dans les années avenir la facon de faire du business sur le web changera profondement. E-artisanat est la pour vous aider à faire la difference
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="main-start-box">
+                <div class="bg_img_left"><img src="{{ url('/images/bg_img1.png') }}" alt="#" /></div>
+                <div class="container">
+                    <div class="buyer-box clearfix">
+                        <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.2s">
+                            <div class="left-buyer">
+                                <img class="img-responsive" src="{{ url('/images/creat_pro.png') }}" alt="#" />
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 wow fadeIn" data-wow-delay="0.4s">
+                            <div class="right-buyer">
+                                <h4>Acheteur</h4>
+                                <h2>Donné du pouvoir à nos artisants<br>
+                                    <span>Avec l'économie collaborative</span>
+                                </h2>
+                                <p> Acheter des produit 100% certifié artisanal. Tous nos artisant sont validés par notre team, et évalué par la communauté.
+                                </p>
+                                <p>Laissez votre avis et partagez vos expérience avec nos artisant à la communauté</p>
+                                <a href="index.php?action=register">Créer un compte acheteur</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="bg_img_right"><img src="{{ url('/images/bg_img1.png') }}" alt="#" /></div>
+            <div class="main-start-box">
+                <div class="container">
+                    <div class="supplier clearfix">
+                        <div class="col-md-5 col-sm-6">
+                            <div class="left-supplier">
+                                <h4>Vendeur</h4>
+                                <h2>Augmenter vos revenus <br><span>Avec l'économie collaborative</span></h2>
+                                <p>Ne vous inquiéter plus pour vos fin de mois et gagnez jusqu'à plusieur centaine d'euro par mois.
+                                </p>
+                                .
+                                <a href="index.php?action=registe">Creer un acompte vendeur</a>
+                            </div>
+                        </div>
+                        <div class="col-md-7 col-sm-6">
+                            <div class="right-supplier">
+                                <img class="img-responsive" src="{{ url('/images/supplier-pc-img.png') }}" alt="#" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+main js-->
+   <script src="./js/jquery-1.12.4.min.js"></script>
+   bootstrap js
+   <script src="./js/bootstrap.min.js"></script>
+   <script src="./js/bootstrap-select.min.js"></script>
+   <script src="./js/slick.min.js"></script>
+   <script src="./js/wow.min.js"></script>
+   custom js
+<script src="./js/custom.js"></script>
+@endsection
